8b0f4fb176b4b25a4d22ca4cbf2e43e85010a302
Kat Leight <leightkt@gmail.com>
2020-12-17 16:48:43 -0700

    Done.

--- app.sql
+++ app.sql
@@ -1,14 +1,23 @@
 -- Create a table, `owners`
-
+CREATE TABLE owners (id INTEGER PRIMARY KEY, name TEXT);
 -- Create a related table, `bicycles`
-
+CREATE TABLE bicycles (id INTEGER PRIMARY KEY, brand TEXT, color TEXT, owner_id);
 -- Create 3 bicycles and 2 owners
-
+INSERT INTO bicycles (brand, color) VALUES ('Quintanaroo', 'blue'), ('Trek', 'red'), ('Bianchi', 'silver');
+INSERT INTO owners (name) VALUES ('Kat'), ('Sam');
+UPDATE bicycles SET owner_id = 1 WHERE brand = 'Quintanaroo';
+UPDATE bicycles SET owner_id = 1 WHERE brand = 'Bianchi';
+UPDATE bicycles SET owner_id = 2 WHERE brand = 'Trek';
 -- Update the properties of one of the bicycles
-
+UPDATE bicycles SET brand = 'Cannondale' WHERE brand = 'Quintanaroo';
+UPDATE bicycles SET color = 'orange' WHERE brand = 'Cannondale';
 -- List all the bicycles
+SELECT * FROM bicycles;
 
 -- List all the bicycles with their owners
+SELECT bicycles.brand, owners.name 
+FROM bicycles 
+INNER JOIN owners
+ON owners.id = bicycles.owner_id;
 
--- Delete one of the bicycles
-
+DELETE FROM bicycles WHERE brand = 'Bianchi';
